06c7c77e19eec31da9952e1a0cd07f83349c2992
lukesj28 <lukesj28@gmail.com>
2025-12-15 20:41:19 -0500

    feat: remove clahe

--- preprocessing/filters.py
+++ preprocessing/filters.py
@@ -10,18 +10,3 @@ def apply_unsharp_mask(image, strength=2.0):
     sharpened = cv2.addWeighted(image, 1.0 + strength, blur, -strength, 0)
     
     return sharpened
-
-def apply_clahe(image, clip_limit=2.0, tile_grid_size=(8,8)):
-    # Convert to LAB (Lightness serves as intensity)
-    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-    l, a, b = cv2.split(lab)
-    
-    # Apply CLAHE to L-channel
-    clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
-    cl = clahe.apply(l)
-    
-    # Merge and Convert back
-    limg = cv2.merge((cl, a, b))
-    final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-    
-    return final
